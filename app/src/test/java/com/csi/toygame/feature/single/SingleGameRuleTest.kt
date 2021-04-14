47aa55aafd2a7020bbc1209662cab6600e104a48
Robin <robin@daangn.com>
2021-04-15 00:21:09 +0900

    싱글플레이 - 게임룰 TC 변경

--- app/src/test/java/com/csi/toygame/feature/single/SingleGameRuleTest.kt
+++ app/src/test/java/com/csi/toygame/feature/single/SingleGameRuleTest.kt
@@ -1,7 +1,7 @@
 package com.csi.toygame.feature.single
 
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.equalTo
 import org.junit.Test
 
 class SingleGameRuleTest {
@@ -10,13 +10,13 @@ class SingleGameRuleTest {
 
     @Test
     fun 게임룰_1부터_100까지_안에_숫자가_속해있지않으면_게임룰에_어긋나는것으로_판단된다() {
-        assertThat(gameRule.isCanGuessNumber(101, 1), Matchers.equalTo(false))
-        assertThat(gameRule.isCanGuessNumber(-100, 1), Matchers.equalTo(false))
+        assertThat(gameRule.isCanGuessNumber(101, 1), equalTo(false))
+        assertThat(gameRule.isCanGuessNumber(-100, 1), equalTo(false))
     }
 
     @Test
     fun 게임룰_1부터_100까지_안에_숫자가_속해있으면_게임룰이_맞다는걸_판단한다() {
-        assertThat(gameRule.isCanGuessNumber(99, 1), Matchers.equalTo(true))
-        assertThat(gameRule.isCanGuessNumber(1, 1), Matchers.equalTo(true))
+        assertThat(gameRule.isCanGuessNumber(99, 1), equalTo(true))
+        assertThat(gameRule.isCanGuessNumber(1, 1), equalTo(true))
     }
 }
